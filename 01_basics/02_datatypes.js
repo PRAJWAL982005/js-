1f1b9452d3fec7e8f5c6583f69394d52d2434e3d
PRAJWAL982005 <162725373+PRAJWAL982005@users.noreply.github.com>
2024-08-12 05:12:50 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all js code as newer version
+
+// alert()// we are using node.js ,not browser
+
+// primitive 
+
+// number => 2 to power 53
+// bigint
+// string "" or ''
+// boolean
+// null  => standalone value & it is an object
+// undefined => value is not assigned
+// symbol => unique
+
+// object
+console.log(typeof null)
+console.log(typeof undefined)
+
