29a432797f74b632904b64134d3887010e2b1b6b
PRAJWAL982005 <162725373+PRAJWAL982005@users.noreply.github.com>
2024-08-12 06:40:39 +0000

    data type summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,27 @@
+/*
+        primitive data types -> are call by value
+
+        7 types : string,Number,Boolean,null = empty,undefined = variable is declared but what value it comes it not defined,symbol,bigInt
+
+java script is dynamically typed language
+        non primitive or reference type
+
+        Array,Objects,Functions
+         to declare symbols const id = Symbol('123')
+
+        
+*/
+
+// const id = Symbol('123') != const id1 = Symbol('123')
+
+
+const heros = ["a","b","c"] //-> Array 
+
+let myObj = {
+        name: "prajwal",    // ->objects
+        age:19,
+}
+
+const myFunction = function(){
+        console.log("hai")
+}
