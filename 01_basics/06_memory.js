306a0339c7a1324fd572950c6534d2ca4e1e3fba
PRAJWAL982005 <162725373+PRAJWAL982005@users.noreply.github.com>
2024-08-13 02:49:09 +0000

    memory

---
+++ 01_basics/06_memory.js
@@ -0,0 +1,31 @@
+// stack(primitve)  , Heap(non-primitive)
+
+
+// example
+
+
+
+/*
+
+
+let myCollege = "sahyadri"
+
+let MyCollege = myCollege
+
+console.log(MyCollege)
+console.log(myCollege)
+
+let user = {
+    email:"user@google.com",
+    upi: "prajwal@",
+}
+
+let user2 = user
+
+user2.email = "prajwal"
+
+console.log(user2)
+console.log(user)
+   in stacks we get a copy of the variable , whenever we get any value in heap we get direct reference     
+
+*/
