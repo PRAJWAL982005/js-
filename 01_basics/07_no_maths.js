8eb569018d13261186a0bc7f7a1f5b95f702b34d
PRAJWAL982005 <162725373+PRAJWAL982005@users.noreply.github.com>
2024-08-14 14:33:56 +0530

    Update 07_no_maths.js

--- 01_basics/07_no_maths.js
+++ 01_basics/07_no_maths.js
@@ -1,49 +1,48 @@
-// const score = 400.98643
+const score = 400.98643
 
-// const balance = new Number(100.8865)// to specify the data type
+const balance = new Number(100.8865)// to specify the data type
 
-// console.log(score)
+console.log(score)
 
-// console.log(balance.toString()) //to conver to the string
+console.log(balance.toString()) //to conver to the string
 
-// console.log(balance.toFixed(2))  //to fix the decimal places
+console.log(balance.toFixed(2))  //to fix the decimal places
 
-// const otherNumber = 123.896
-// console.log(otherNumber.toPrecision(3)) 
+const otherNumber = 123.896
+console.log(otherNumber.toPrecision(3)) 
 
-// console.log(typeof balance)
-// const rupees = 10000000000
-// console.log(rupees.toLocaleString('en-IN'))  //to put commas so as to show the rupees
-
-
-
-// *********************************************maths**************************************************************
+console.log(typeof balance)
+const rupees = 10000000000
+console.log(rupees.toLocaleString('en-IN'))  //to put commas so as to show the rupees
 
 
 
-// console.log(Math);
+*********************************************maths**************************************************************
 
-// console.log(Math.abs(-4)); //->changing negative to positive
 
-// console.log(Math.round(6.9));
+console.log(Math);
 
-// console.log(Math.ceil(6.9));
+console.log(Math.abs(-4)); //->changing negative to positive
 
-// console.log(Math.floor(6.9));
+console.log(Math.round(6.9));
 
-// console.log(Math.sqrt(6.9));
+console.log(Math.ceil(6.9));
 
-// console.log(Math.random()); //always give values between 0 and 1
+console.log(Math.floor(6.9));
 
-// console.log((Math.random()*10) + 1);  //to get value above 1
+console.log(Math.sqrt(6.9));
 
-// console.log((Math.random()*10) + 1);  //will give till 9
+console.log(Math.random()); //always give values between 0 and 1
 
-// const min = 10
+console.log((Math.random()*10) + 1);  //to get value above 1
 
-// const max = 20
+console.log((Math.random()*10) + 1);  //will give till 9
 
-// console.log(Math.floor((Math.random() * (max-min+1)))+min)
+const min = 10
+
+const max = 20
+
+console.log(Math.floor((Math.random() * (max-min+1)))+min)
 
 
 
