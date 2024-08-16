44278ba284fc56c2b6b3fe14948e43440a5f3093
PRAJWAL K K <162725373+prajwal-kk98@users.noreply.github.com>
2024-08-16 22:08:03 +0530

    Create while&dowhile.js

---
+++ 07_controlflow/while&dowhile.js
@@ -0,0 +1,10 @@
+let i = 0
+while (i<=10) {
+    console.log(`value of index ${i}`); //${myarray[arr]}  for arrays
+    i = i+2
+}
+
+do {
+    console.log(`value of index ${i}`); //${myarray[arr]}  for arrays
+    i = i+2
+} while (i<=10);
